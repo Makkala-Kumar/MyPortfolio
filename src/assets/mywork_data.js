a9618034c96f5e150df02bd86550136e58aefb94
Kumar <makkalakumar55@gmail.com>
2024-05-09 21:03:54 +0530

    updates

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,9 @@
 import project1_img from '../assets/screenTime.png'
 import project2_img from '../assets/tollgate.jpg'
-import project3_img from '../assets/willBeAdded.png'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project3_img from '../assets/ExcelProjects.png'
+import project4_img from '../assets/willBeAdded.png'
+import project5_img from '../assets/willBeAdded.png'
+import project6_img from '../assets/willBeAdded.png'
 
 const mywork_data = [
     {
@@ -20,15 +20,16 @@ const mywork_data = [
     },
     {
         w_no:3,
-        w_name:"To be added",
+        w_name:"Excel Projects",
         w_img:project3_img,
-        w_link :'https://github.com/Makkala-Kumar/ScreenTime'
+        w_link :'https://github.com/Makkala-Kumar/My-Excel-Projects/tree/main'
     },
-    // {
-    //     w_no:4,
-    //     w_name:"Web design",
-    //     w_img:project4_img
-    // },
+    {
+        w_no:4,
+        w_name:"To be added",
+        w_img:project4_img,
+        w_link :''
+    },
     // {
     //     w_no:5,
     //     w_name:"Web design",
