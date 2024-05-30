5638f52dadebea5fc8b2b10f9c92cb84ba3228cc
Kumar <makkalakumar55@gmail.com>
2024-05-30 12:32:24 +0530

    added resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Resume from '../../assets/resume.pdf'
 
 const Hero = () => {
   return (
@@ -12,10 +13,10 @@ const Hero = () => {
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me </AnchorLink></div>
         <div className="hero-resume">
-          <a  href="../../assets/resume.pdf" download="Kumar_Makkala_Resume.pdf">Download My Resume</a></div>
+          <a  href={Resume} download="Kumar_Makkala_Resume.pdf">Download My Resume</a></div>
       </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero 
