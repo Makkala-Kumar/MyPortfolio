b5e861a6b1ae2061fdcad27a7fe9b40f19e5a3b8
Kumar <makkalakumar55@gmail.com>
2024-05-30 11:44:51 +0530

    updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,7 +11,8 @@ const Hero = () => {
       <p>Contact me for business related and download my resume</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me </AnchorLink></div>
-        <div className="hero-resume">My Resume</div>
+        <div className="hero-resume">
+          <a  href="../../assets/Resume.pdf" download="Resume.pdf">My Resume</a></div>
       </div>
     </div>
   )
